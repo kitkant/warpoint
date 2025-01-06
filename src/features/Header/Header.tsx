bc984d2c5251b7c2cec19283f988a0c20a975d7e
Nikita <nikitadyshkant@gmail.com>
2025-01-06 14:19:08 +0300

    change links

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -2,15 +2,16 @@
 import React from 'react';
 import styles from './styles/header.module.css'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header = () => {
 	return (
 		<header className={styles.header}>
 		<div className={styles.wrapper}>
 			<div className={styles.nav_containerLogo}>
-				<a href="/pages/contacts.html">
+				<Link href="/">
 				<Image width={120} height={17} src={'/img/pages/contacts/logo.svg'} alt='logo'/>
-				</a>
+				</Link>
 				<div className={styles.nav_containerLogo__dropdown}>
 					<div className={styles.nav_containerLogo__container}>
 						<button className={styles.nav_containerLogo__textCity}>Москва</button>
@@ -18,87 +19,87 @@ const Header = () => {
 					</div>
 					<div className={styles.nav_containerLogo__downList}>
 						<div className={styles.nav_containerLogo__triangle}></div>
-						<a href="#">Выберите другой город</a>
+						<Link href="/">Выберите другой город</Link>
 					</div>
 				</div>
 			</div>
 			<nav className={styles.nav_container__navigationContainer}>
 				<ul className={styles.nav_container__navigation}>
 					<li className={styles.nav_container__navigationItem}>
-						<a href="#" className={styles.nav_container__navigationItem__link}>Тарифы</a>
+						<Link href="/" className={styles.nav_container__navigationItem__link}>Тарифы</Link>
 					</li>
 					<li className={styles.nav_container__navigationItem}>
-						<a href="#" className={styles.nav_container__navigationItem__link}>Наши игры</a>
+						<Link href="/" className={styles.nav_container__navigationItem__link}>Наши игры</Link>
 					</li>
 					<li className={styles.nav_container__navigationItem + ' ' + styles.nav_containerNavigation__dropdown}>
 						<div className={styles.nav_containerNavigation__container}>
-							<a href="#" className={styles.nav_container__navigationItem__link}>Мероприятия</a>
+							<Link href="/" className={styles.nav_container__navigationItem__link}>Мероприятия</Link>
 							<div className={styles.nav_containerNavigation__triangle}></div>
 						</div>
 						<div className={styles.nav_containerNavigation__downList}>
 							<div className={styles.nav_containerNavigation__triangleBorder}></div>
 							<ul className={styles.nav_container__downContainer__navigation}>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Детский день рождения</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Детский день рождения</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Взрослый день рождения</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Взрослый день рождения</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Корпоратив</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Корпоратив</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Школьный выпускной</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Школьный выпускной</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Турниры</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Турниры</Link>
 								</li>
 							</ul>
 						</div>
 					</li>
 					<li className={styles.nav_container__navigationItem}>
-						<a href="#" className={styles.nav_container__navigationItem__link}>Сертификаты</a>
+						<Link href="/" className={styles.nav_container__navigationItem__link}>Сертификаты</Link>
 					</li>
 					<li className={styles.nav_container__navigationItem + ' ' + styles.nav_containerNavigation__dropdown}>
 						<div className={styles.nav_containerNavigation__container}>
-							<a href="#" className={styles.nav_container__navigationItem__link}>
+							<Link href="/" className={styles.nav_container__navigationItem__link}>
 								Информация
-							</a>
+							</Link>
 							<div className={styles.nav_containerNavigation__triangle}></div>
 						</div>
 						<div className={styles.nav_containerNavigation__downList}>
 							<div className={styles.nav_containerNavigation__triangleBorder}></div>
 							<ul className={styles.nav_container__downContainer__navigation}>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Клубная карта</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Клубная карта</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Наши акции</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Наши акции</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Новости</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Новости</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Полезные статьи</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Полезные статьи</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Франшиза</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Франшиза</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Витрина</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Витрина</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>Аренда</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>Аренда</Link>
 								</li>
 								<li className={styles.nav_container__downContainer__navigationItem}>
-									<a href="#" className={styles.nav_container__downContainer__navigationItem__link}>О нас</a>
+									<Link href="/" className={styles.nav_container__downContainer__navigationItem__link}>О нас</Link>
 								</li>
 							</ul>
 						</div>
 
 					</li>
 					<li className={styles.nav_container__navigationItem}>
-						<a href="#" className={styles.nav_container__navigationItem__link}>Контакты</a>
+						<Link href="/" className={styles.nav_container__navigationItem__link}>Контакты</Link>
 					</li>
 				</ul>
 			</nav>
