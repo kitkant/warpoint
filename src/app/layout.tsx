1a4b2583b5b2aa08d0c903d392370f8f76e43515
Nikita <nikitadyshkant@gmail.com>
2025-01-06 14:14:31 +0300

    add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,42 @@
-"use client"
-import localFont from 'next/font/local'
+'use client'
 import Header from '@/features/Header/Header'
-import "./globals.css";
 import styled from '@emotion/styled'
+import localFont from 'next/font/local'
+import './globals.css'
 
-
-const gilroy = localFont({
-  src: [
-    {
-      path: '/fonts/Gilroy-Regular.woff',
-      weight: '400',
-      style: 'normal',
-    },
-
-    {
-      path: '/fonts/Gilroy-Bold.woff',
-      weight: '700',
-      style: 'normal',
-    }
-  ],
-})
+const gilroy = localFont({ src: [
+  {
+    path: '../app/fonts/Gilroy-Regular.woff2',
+    weight: '400'
+  },
+  {
+    path: '../app/fonts/Gilroy-Bold.woff2',
+    weight: '700'
+  }
+] })
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <head>
-   
-    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Контакты VR-арены WARPOINT в Москве м. Волгоградский пр-кт</title>
-    </head>
-      <Body  className={gilroy.className}>
-      <Header/>
-        {children}
-      </Body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<head>
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+				<meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+				<title>
+					Контакты VR-арены WARPOINT в Москве м. Волгоградский пр-кт
+				</title>
+			</head>
+			<Body className={gilroy.className}>
+				<Header />
+				{children}
+			</Body>
+		</html>
+	)
 }
 
 const Body = styled.body`
-  background-color: rgb(27, 27, 27);
+	background-color: rgb(27, 27, 27);
 `
