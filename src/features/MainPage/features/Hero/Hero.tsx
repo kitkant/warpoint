c0ae4f3ecc8ea40f884af263bc89ddd12418afee
Nikita <nikitadyshkant@gmail.com>
2025-02-11 11:15:46 +0300

    adaptive for xl

--- src/features/MainPage/features/Hero/Hero.tsx
+++ src/features/MainPage/features/Hero/Hero.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import Button from '@/app/UI/Button/Button'
 import Image from 'next/image'
 import styles from '../../styles/main.module.css'
@@ -8,6 +8,7 @@ import useMediaQuery from '@/app/hooks/useMediaQuery'
 const Hero = () => {
 
 	const desktop1710 = useMediaQuery(1711)
+	const desktop1280 = useMediaQuery(1280 + 21)
 
 	return (
 		<div className={styles.wrapper}>
@@ -17,15 +18,15 @@ const Hero = () => {
 						<Image
 							className={styles.groundImage}
 							src={'/img/pages/main/ground.png'}
-							width={900}
-							height={600}
+							width={desktop1280 ? 800  :900}
+							height={desktop1280 ? 520  :600}
 							alt='ground'
 						/>
 						<Image
 							className={styles.teamImage}
 							src={'/img/pages/main/team.png'}
-							width={790}
-							height={1150}
+							width={desktop1280 ? 700 :790}
+							height={desktop1280 ? 1000 : 1150}
 							alt='ground'
 						/>
 						<div className={styles.gradientBlackGround}></div>
